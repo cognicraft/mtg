6e85fe2b8cc7b8c14e9179b46f1eb21e02f168b2
Mario Demuth <mario.demuth@voestalpine.com>
2020-02-17 21:31:15 +0100

    updated

--- cmd/mtg-tools/main.go
+++ cmd/mtg-tools/main.go
@@ -102,7 +102,6 @@ body {
 	font-family: "Roboto", Helvetica, Arial, sans-serif;
 	font-weight: 100;
 	font-size: 12px;
-	line-height: 30px;
 	color: #777;
 	background: #4CAF50;
 	margin: 4em;
@@ -110,9 +109,11 @@ body {
 
 .card {
 	background: #F9F9F9;
-	width: 50%;
+	width: 75%;
 	padding: 25px;
-	margin: auto auto;
+	margin: auto;
+	margin-top: 3em;
+	margin-bottom: 3em;
 	border-radius: 10px;
 	box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
 }
@@ -120,9 +121,21 @@ body {
 	margin-bottom: 1em;
 }
 
+.card ul {
+	margin-top: .75em;
+	margin-left: 1em;
+	margin-right:1em;
+	margin-bottom: .75em;
+}
+
+.card li {
+	margin-top: .5em;
+	margin-bottom: .5em;
+}
+
 textarea {
 	margin: auto;
-	width: 98%;
+	width: 99%;
 	resize: none;
 }
 `
@@ -147,6 +160,22 @@ const index = `
 			<textarea name="deck" cols="80" rows="30"></textarea><br/>
 			<input type="submit" value="Generate Proxies" />
 		</form>
+	</div>
+	<div class="card">
+		<h1>The Staples Binder Method</h1>
+		<p>The Staples Binder Method can be used to to save some cash while playing multiple decks within a format. With this method you will need at max 4 original copies of any given card in your collection. To reduce the amount of effort this method should only be used for cards that have a value greater than a few dollars.</p>
+		<ul>
+			<li>Put all decks in the same sleeves (brand/color).</li>
+			<li>Keep the sleeved staples in a binder.</li>
+			<li>Proxy print the needed number of any given staple with the decks name as an overlay.</li>
+			<li>Cut out the proxy.</li>
+			<li>Sleve up the proxy together with a basic land.</li>
+			<li>Keep the sleeved proxy with the rest of the cards in the deck.</li>
+			<li>Before the game pull out all proxies and replace them with the original printings from the Stapels Binder.</li>
+			<li>After the game replace all of the staples in your deck with the proxies that where pulled out before the game.</li>
+			<li><em>Tip:</em> The name of the deck is important if you want to always know where the original printing currently is. If you find a proxy within your Staples Binder the name will tell you where your original is located. In case you're trying to replace a proxy from deck A and find a proxy of deck B in your Staples Binder you know that the original is currently located in deck B.</li>
+			<li><em>Tip:</em> In a casual setting and if your playgroup does not mind, you could skip replacing the proxies to save some time and effort. In case you are double sleeving your cards you will not notice a difference in thickness between (proxy + basic land + outer-sleeve) and (inner-sleeve + staple + outer-sleeve).</li>
+		</ul>
 	</div>
 </body>
 </html>
